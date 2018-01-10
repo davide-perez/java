107353d0c2f7eb3e947cfdc3af888eb1b7e286e5
davide-perez <34714232+davide-perez@users.noreply.github.com>
2018-01-10 09:42:15 +0100

    deserialization implementation and other changes
    Implemented deserialize method: a string object is read from the file in which it was serialized and printed
    Modified serialize method: serialization makes sense to store an object to a file. The file where the serialized object has to be saved is provided along with the string to serialize. The string is then serialized and stored in such file.

--- serialize/Serialize.java
+++ serialize/Serialize.java
@@ -14,20 +14,50 @@ public class Serialize
 
 		System.out.print("\n");
 	}
-	public static void serialize(String data)
+	public static void serialize(String data, String filename)
 	{
 		try
 		{
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			oos.writeObject(data);
-			oos.flush();
-			System.out.print(baos.toString() + "\n");
+			//Save object in a file
+           		FileOutputStream fos = new FileOutputStream(filename);
+           	 	ObjectOutputStream out = new ObjectOutputStream(fos);
+			out.writeObject(data);
+			out.flush();
+			System.out.print("Object serialized in file " + filename);
+			//System.out.print(out.toString() + "\n");
 		}
 		catch (Exception e)
 		{
-			System.out.print(e + "\n");
+			System.out.println(e);
 		}
 	}
-	public static void deserialize(String data) {}
+	public static void deserialize(String filename) {
+	
+		String s = null;
+ 
+        
+        	try
+        	{   
+            	// Read the object from a file
+            	FileInputStream file = new FileInputStream(filename);
+            	ObjectInputStream in = new ObjectInputStream(file);
+             
+            	// Method for deserialization of object
+            	s = (String)in.readObject();
+             
+            	in.close();
+            	file.close();
+             	
+		//print result
+            	System.out.println("Object has been deserialized ");
+            	System.out.println("Deserialized string = " + s);
+        }
+         
+        catch(Exception ex)
+        {
+            System.out.println(ex);
+        }
+	
+	
+	}
 }
